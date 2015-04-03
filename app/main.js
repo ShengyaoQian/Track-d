68a101577aeeed1b4be6d39a50ae52f7a92a6ba7
Shengyao Qian <qians2@uw.edu>
2015-04-02 21:24:05 -0700

    jsonify response

--- app/main.js
+++ app/main.js
@@ -14,11 +14,11 @@ var models = require('./models');
 app.get('/events', function(req, res) {
     var Events = require('./models').Events;
     var events = new Events();
-    var json_response = [];
+    var json_response = {'events': []};
 	events.fetch().then(function(collection) { 
     	collection.forEach(function (model) {
         	var json_obj = {'event_id': model.get('event_id')};
-        	json_response.push(json_obj);
+        	json_response.events.push(json_obj);
         });
         res.send(json_response);
     }); 
