0056fd79e5f6eb984ef14f84ceb72174219c4370
Shengyao Qian <qians2@uw.edu>
2015-04-06 22:06:37 -0700

    Change schema

--- migrations/20150301190131_001_migration.js
+++ migrations/20150301190131_001_migration.js
@@ -4,32 +4,39 @@
 */
 
 exports.up = function(knex, Promise) {
-    return Promise.all([
-        /* Create table Event table. */
+    return Promise.all([      
+        /* Create Location table. */
+        knex.schema.createTable('Location', function (table) {
+            table.bigIncrements('location_id').primary().unsigned().index();
+            table.string('name', 50);
+            table.string('address', 50);
+            table.float('lat');
+            table.float('log');
+        }),
+
+        /* Create Event table. */
         knex.schema.createTable('Event', function (table) {
             table.bigIncrements('event_id').primary().unsigned().index();
+            table.bigInteger('location_id').unsigned().notNullable().unique().references('location_id').inTable('Location');
             table.string('event_name', 50);
             table.dateTime('event_time');
-            table.string('event_location');
-            table.string('event_desc');
+            table.string('event_desc_short');
+            table.string('event_desc_long');
             table.boolean('event_recur');
-        }),
-        
-        /* Create OrgApproved table which contains a list of email of approved organizations. */ 
-        knex.schema.createTable('OrgApproved', function (table) {
-            table.string('email').primary();
         }),
         
         /* Create Organization table. */ 
         knex.schema.createTable('Organization', function (table) {
             table.bigIncrements('org_id').primary().unsigned();
+            table.bigInteger('location_id').unsigned().notNullable().unique().references('location_id').inTable('Location');
             table.string('user_name').notNullable().index();
             table.string('name').notNullable();
-            table.string('email').notNullable().unique().references('email').inTable('OrgApproved');
+            table.string('email').notNullable();
             table.string('org_location');
             table.string('website');
             table.string('facebook');
             table.string('org_desc');
+            table.boolean('approved');
         }),
         
         /* Create Organization to Event table. */  
@@ -48,30 +55,32 @@ exports.up = function(knex, Promise) {
         /* Create Place table. */ 
         knex.schema.createTable('Place', function (table) {
             table.bigIncrements('place_id').primary().unsigned();
+            table.bigInteger('location_id').unsigned().notNullable().unique().references('location_id').inTable('Location');
             table.bigInteger('org_id').unsigned().notNullable().unique().references('org_id').inTable('Organization');
             table.bigInteger('type_id').unsigned().notNullable().unique().references('type_id').inTable('Place_Type');
             table.string('place_name');
-            table.string('place_location');
             table.dateTime('open_time');
             table.dateTime('close_time');
             table.string('place_desc');
         }),
         
-        /* Create Login table. */
-        knex.schema.createTable('Login', function (table) {
-            table.bigIncrements('user_id').primary().unsigned();
-            table.string('user_name').notNullable().unique().references('user_name').inTable('Organization');
-            table.string('password').notNullable();
-        }),
-        
         /* Create Person table. */
         knex.schema.createTable('Person', function (table) {
             table.bigIncrements('person_id').primary().unsigned();
-            table.string('user_name').notNullable().unique().references('user_name').inTable('Login');
+            table.string('user_name');
             table.string('user_email');
             table.string('first_name');
             table.string('last_name');
             table.string('major');
+        }),
+
+        /* Create Person to Org table. */
+        knex.schema.createTable('Person_Org', function (table) {
+            table.bigInteger('person_id').unsigned().notNullable().unique().references('person_id').inTable('Person');
+            table.bigInteger('org_id').unsigned().notNullable().unique().references('org_id').inTable('Organization');
+            table.primary(['person_id', 'org_id']);
+            table.string('user_name').notNullable().unique().references('user_name').inTable('Organization');
+            table.string('password').notNullable();
         }),
         
         /* Create Tag table */
@@ -84,22 +93,34 @@ exports.up = function(knex, Promise) {
         knex.schema.createTable('Event_Tag', function (table) {
             table.bigInteger('tag_id').unsigned().notNullable().unique().references('tag_id').inTable('Tag');
             table.bigInteger('event_id').unsigned().notNullable().unique().references('event_id').inTable('Event');
-        }) 
+        }),
+
+        knex.schema.createTable('Major', function (table) {
+            table.bigIncrements('major_id').primary().unsigned().index();
+            table.string('name');
+        }),
+
+        knex.schema.createTable('Person_Major', function (table) {
+            table.bigInteger('person_id').unsigned().notNullable().unique().references('person_id').inTable('Person');
+            table.bigInteger('major_id').unsigned().notNullable().unique().references('major_id').inTable('Major');
+        })
 
     ]);  
 };
 
 exports.down = function(knex, Promise) {
     return Promise.all([
+        knex.schema.dropTable('Person_Major'),
+        knex.schema.dropTable('Major'),
         knex.schema.dropTable('Event_Tag'),
         knex.schema.dropTable('Tag'),
+        knex.schema.dropTable('Person_Org'),
         knex.schema.dropTable('Person'),
-        knex.schema.dropTable('Login'),
         knex.schema.dropTable('Place'),
         knex.schema.dropTable('Place_Type'),
         knex.schema.dropTable('Org_Event'),
         knex.schema.dropTable('Organization'),
-        knex.schema.dropTable('OrgApproved'),
-        knex.schema.dropTable('Event')
+        knex.schema.dropTable('Event'),
+        knex.schema.dropTable('Location')
     ]);
 };
