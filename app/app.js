cc55d8fee12fb14accceb0ea5b3b56406a83049e
Shengyao Qian <qians2@uw.edu>
2015-04-16 21:38:42 -0700

    Small fixes

--- app/app.js
+++ app/app.js
@@ -5,8 +5,11 @@ app.get('/', function(req, res) {
     res.send('hello world');
 });
 
-app.listen(3000);
-console.log('Server running at http://localhost:3000');
-
 app.get('/events', require('./controllers.js').events);
 
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Server running at http://%s:%s', host, port);
+});
