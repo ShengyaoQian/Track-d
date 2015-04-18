5c4772fae26b77b0f001f7e04ee067978b3ce3e0
Shengyao Qian <qians2@uw.edu>
2015-04-18 14:20:10 -0700

    Modify org route so it returns org with requested id if id param is passed.

--- app/controllers.js
+++ app/controllers.js
@@ -16,6 +16,9 @@ function get_events_info(json_response, callback_counts, callback) {
     var events = new Events();
     events.fetch().then(function (collection) { 
         var collection_size = Object.keys(collection.models).length;
+        if (collection_size == 0) {
+                callback(json_response);
+        };
         async.each(collection.models, function (model) {                      
             var json_obj = {'event_id': model.get('event_id'),
                             'event_name': model.get('event_name'),
@@ -26,7 +29,7 @@ function get_events_info(json_response, callback_counts, callback) {
             var Location = require('./models').Location;
             var location = new Location();
             var loc_info = null; 
-            location.fetch({'location_id': model.get('location_id')}).then(function (model) {
+            location.query({where: {'location_id': model.get('location_id')}}).fetch().then(function (model) {
                 loc_info = {'location_id': model.get('location_id'),
                             'name': model.get('name'),
                             'address': model.get('address'),
@@ -46,47 +49,67 @@ function get_events_info(json_response, callback_counts, callback) {
 exports.organizations = function(req, res) {
     var json_response = {'organizations': []};
     var callback_counts = 0;
-    get_organization_info(json_response, callback_counts, function() {
+    var org_id = req.query.org_id;
+    get_organization_info(json_response, org_id, callback_counts, function() {
         res.setHeader("Access-Control-Allow-Origin", "*");
         res.send(json_response);
     });
 };
 
-function get_organization_info(json_response, callback_counts, callback) {
-    var Organizations = require('./models').Organizations;
-    var organizations = new Organizations();
-    organizations.fetch().then(function (collection) { 
-        var collection_size = Object.keys(collection.models).length;
-        if (collection_size == 0) {
-            callback(json_response);
-        };
-        async.each(collection.models, function (model) {                      
-            var json_obj = {'org_id': model.get('org_id'),
-                            'name': model.get('name'),
-                            'user_name': model.get('user_name'),
-                            'email': model.get('email'),
-                            'website': model.get('website'),
-                            'facebook': model.get('facebook'),
-                            'org_desc': model.get('org_desc'),
-                            'approved': model.get('approved')};
-            var Location = require('./models').Location;
-            var location = new Location();
-            var loc_info = null; 
-            location.fetch({'location_id': model.get('location_id')}).then(function (model) {
-                loc_info = {'location_id': model.get('location_id'),
-                            'name': model.get('name'),
-                            'address': model.get('address'),
-                            'lat': model.get('lat'),
-                            'log': model.get('log')};
-                json_obj['loc_info'] = loc_info;
-                json_response.organizations.push(json_obj);
+function get_organization_info(json_response, org_id, callback_counts, callback) {
+    if (org_id) {
+        var Organization = require('./models').Organization;
+        var organization = new Organization();
+        organization.query({where: {'org_id': org_id}}).fetch().then(function (model) {
+            if (model) {
                 callback_counts ++;
-                if (callback_counts == collection_size) {
-                    callback(json_response);
-                };
+                build_org_json_object(model, callback_counts, 1, json_response, callback);
+            } else {
+                callback(json_response);
+            } 
+        });
+    } else {
+        var Organizations = require('./models').Organizations;
+        var organizations = new Organizations();
+        organizations.fetch().then(function (collection) { 
+            var collection_size = Object.keys(collection.models).length;
+            if (collection_size == 0) {
+                callback(json_response);
+            };
+            async.each(collection.models, function (model) {  
+                callback_counts ++;
+                build_org_json_object(model, callback_counts, collection_size, json_response, callback);            
             });
         });  
-    }); 
+    };
+    
+};
+
+function build_org_json_object(model, callback_counts, collection_size, json_response, callback) {
+    var json_obj = {'org_id': model.get('org_id'),
+                    'name': model.get('name'),
+                    'user_name': model.get('user_name'),
+                    'email': model.get('email'),
+                    'website': model.get('website'),
+                    'facebook': model.get('facebook'),
+                    'org_desc': model.get('org_desc'),
+                    'approved': model.get('approved')};
+    var Location = require('./models').Location;
+    var location = new Location();
+    var loc_info = null; 
+    location.query({where: {'location_id': model.get('location_id')}}).fetch().then(function (model) {
+        loc_info = {'location_id': model.get('location_id'),
+                    'name': model.get('name'),
+                    'address': model.get('address'),
+                    'lat': model.get('lat'),
+                    'log': model.get('log')};
+        json_obj['loc_info'] = loc_info;
+        json_response.organizations.push(json_obj);
+        if (callback_counts == collection_size) {
+            callback(json_response);
+        };
+    });
+
 };
 
 
